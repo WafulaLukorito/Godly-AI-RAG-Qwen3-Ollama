a6c8d5af46cf37f7f893bd0cf58a7556d6f0438e
Lukorito <jonesdeelder@gmail.com>
2025-07-16 08:12:25 +0300

    add top-level init with  app info

---
+++ __init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.1.0"
+__author__ = "Lukorito"
+__email__ = "jonesdeelder@gmail.com"
+
+# setting up package-wide logging
+# from .utils.logging import setup_logging
+# setup_logging()
